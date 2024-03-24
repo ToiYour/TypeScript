5753925d30f25f6c59a1a385b5229378ac8bb039
ToiYour <toidz25102004@gmail.com>
2024-03-24 08:55:16 +0700

    Update

--- Assignment/src/App.tsx
+++ Assignment/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./components/Details";
 import Home from "./components/Homes";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail" element={<DetailPage />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ Assignment/src/components/Checkout.tsx
@@ -0,0 +1,127 @@
+import Footer from "./Footers";
+import Header from "./Headers";
+
+const Checkout = () => {
+  return (
+    <>
+      <Header />
+      <div className="max-w-screen-lg mx-auto font-[sans-serif] bg-gray-50 p-6 min-h-screen">
+        <div className="max-w-7xl mx-auto">
+          <h2 className="text-3xl font-extrabold text-[#333] text-center">
+            Thanh toán
+          </h2>
+          <div className="grid lg:grid-cols-3 gap-8 mt-12">
+            <div className="lg:col-span-2">
+              <h3 className="text-xl font-bold text-[#333]">
+                Chọn phương thức thanh toán của bạn
+              </h3>
+              <div className="grid gap-4 sm:grid-cols-2 mt-6">
+                <div className="flex items-center">
+                  <input
+                    type="radio"
+                    className="w-5 h-5 cursor-pointer"
+                    id="card"
+                    defaultChecked=""
+                  />
+                  <label
+                    htmlFor="card"
+                    className="ml-4 flex gap-2 cursor-pointer"
+                  >
+                    <img
+                      src="https://readymadeui.com/images/visa.webp"
+                      className="w-12"
+                      alt="card1"
+                    />
+                    <img
+                      src="https://readymadeui.com/images/american-express.webp"
+                      className="w-12"
+                      alt="card2"
+                    />
+                    <img
+                      src="https://readymadeui.com/images/master.webp"
+                      className="w-12"
+                      alt="card3"
+                    />
+                  </label>
+                </div>
+                <div className="flex items-center">
+                  <input
+                    type="radio"
+                    className="w-5 h-5 cursor-pointer"
+                    id="paypal"
+                  />
+                  <label
+                    htmlFor="paypal"
+                    className="ml-4 flex gap-2 cursor-pointer"
+                  >
+                    <img
+                      src="https://readymadeui.com/images/paypal.webp"
+                      className="w-20"
+                      alt="paypalCard"
+                    />
+                  </label>
+                </div>
+              </div>
+              <form className="mt-8">
+                <div className="grid gap-6">
+                  <div className="grid sm:grid-cols-3 gap-6">
+                    <input
+                      type="number"
+                      placeholder="Card number"
+                      className="px-4 py-3.5 bg-white text-[#333] w-full text-sm border rounded-md focus:border-[#007bff] outline-none"
+                    />
+                    <input
+                      type="number"
+                      placeholder="EXP."
+                      className="px-4 py-3.5 bg-white text-[#333] w-full text-sm border rounded-md focus:border-[#007bff] outline-none"
+                    />
+                    <input
+                      type="number"
+                      placeholder="CVV"
+                      className="px-4 py-3.5 bg-white text-[#333] w-full text-sm border rounded-md focus:border-[#007bff] outline-none"
+                    />
+                  </div>
+                  <div className="sm:col-span-2 grid sm:grid-cols-2 gap-6">
+                    <input
+                      type="text"
+                      placeholder="Name of card holder"
+                      className="px-4 py-3.5 bg-white text-[#333] w-full text-sm border rounded-md focus:border-[#007bff] outline-none"
+                    />
+                    <input
+                      type="number"
+                      placeholder="Postal code"
+                      className="px-4 py-3.5 bg-white text-[#333] w-full text-sm border rounded-md focus:border-[#007bff] outline-none"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="lg:border-l lg:pl-8">
+              <h3 className="text-xl font-bold text-[#333]">Đơn hàng</h3>
+              <ul className="text-[#333] mt-6 space-y-4">
+                <li className="flex flex-wrap gap-4 text-sm">
+                  Giảm giá (20%) <span className="ml-auto font-bold">0đ</span>
+                </li>
+
+                <li className="flex flex-wrap gap-4 text-base font-bold border-t pt-4">
+                  Tổng <span className="ml-auto">0đ</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-4 mt-10">
+            <button
+              type="button"
+              className="px-6 py-3.5 text-sm bg-blue-600 text-white rounded-md hover:bg-blue-700"
+            >
+              Xác nhận
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Checkout;
